5dfbe41f5c4f88996a574c1de9695580cb8d5baa
Zekun Wang <wswzk864864@gmail.com>
2022-06-21 17:06:48 -0400

    remove obsolete code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::io::{stdout, Result};
+use std::io::{Result};
 
-use symbolic_move::{test_fn};
 use clap::Parser;
 /// Symbolic move interpreter
 #[derive(Parser, Debug)]
@@ -16,15 +15,5 @@ struct Args {
 }
 
 fn main() -> Result<()> {
-  let args = Args::parse();
-  let fun_name: Vec<_> = args.target.split("::").collect();
-  if fun_name.len() != 3 {
-    println!("Invalid target name");
-    Ok(())
-  } else {
-  test_fn(&args.file, &[],
-    fun_name[0].to_string(), fun_name[1].to_string(), fun_name[2].to_string(),
-    args.locals,
-    &mut stdout())
-  }
+  Ok(())
 }
